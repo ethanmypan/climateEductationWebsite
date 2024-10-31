b27d9db7d19565155d2989fbd3013137a44744a0
lkimdaryl <lkimdaryl@gmail.com>
2024-10-31 16:02:02 -0700

    setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+import Launch from './pages/launch/launch.jsx'
+import Error from './pages/error/error.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      { path: '/', element: <Launch />} ,
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/launch/launch.jsx
@@ -0,0 +1,9 @@
+import "./launch.css"
+
+export default function Launch() {
+    return (
+        <div className="launch">
+            <h1>Launch</h1>
+        </div>
+    )
+}
