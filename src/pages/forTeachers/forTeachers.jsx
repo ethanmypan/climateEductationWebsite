84475d3bcd36c0f0ebcf554fa4e1eab667fa9970
lkimdaryl <lkimdaryl@gmail.com>
2024-11-08 14:12:41 -0800

    setup the for teachers and activity page

---
+++ src/pages/forTeachers/forTeachers.jsx
@@ -0,0 +1,9 @@
+import './forTeachers.css'
+
+export default function ForTeachers() {
+    return (
+        <div id="for-teachers-page"> 
+            <h1>For Teachers</h1>
+        </div>
+    )
+}
